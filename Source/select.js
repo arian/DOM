8951adbfe88cc650bd63d7bb72596c4e0291e4f3
Arian <arian@aryweb.nl>
2011-09-27 20:49:24 +0200

    Only calculate the specificity one for each wrapper

--- Source/select.js
+++ Source/select.js
@@ -7,35 +7,26 @@ Node.defineMutator('Matches', function(match){
 	matchers.push({
 		match: match,
 		construct: this,
-		type: typeof match
+		specificity: (typeof match == 'string') ? specificity.specificity(match) : -1
 	});
 });
 
 var select = function(node){
-	if (node != null){
-		if (typeof node == 'string') return select(Slick.find(document, node));
-		if (node instanceof Node) return node;
-		var uid = node.uniqueNumber || Slick.uidOf(node), wrapper = wrappers[uid];
-		if (wrapper) return wrapper;
-		var matchedSelectors = [], matchedConstructors = {};
-		var matcher, type, match, construct;
-		for (var l = matchers.length; l--;){
-			matcher = matchers[l];
-			type = matcher.type;
-			match = matcher.match;
-			construct = matcher.construct;
-			var matched = type == 'string' ? Slick.match(node, match) : match(node);
-			if (matched){
-				if (type != 'string') return (wrappers[uid] = new construct(node));
-				matchedConstructors[match] = construct;
-				matchedSelectors.push(match);
-			}
-		}
-		if (matchedSelectors.length){
-			construct = matchedConstructors[specificity.max(matchedSelectors)];
-			if (construct) return (wrappers[uid] = new construct(node));
+	if (node == null) return null;
+	if (typeof node == 'string') return select(Slick.find(document, node));
+	if (node instanceof Node) return node;
+	var uid = node.uniqueNumber || Slick.uidOf(node), wrapper = wrappers[uid];
+	if (wrapper) return wrapper;
+	var construct, specific = 0;
+	for (var l = matchers.length; l--;){
+		var match = matchers[l].match, s = matchers[l].specificity;
+		var matched = s != -1 ? Slick.match(node, match) : match(node);
+		if (matched && (s == -1 || s >= specific)){
+			construct = matchers[l].construct;
+			if (s == -1) break;
 		}
 	}
+	if (construct) return (wrappers[uid] = new construct(node));
 	return null;
 };
 
