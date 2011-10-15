07d45c3c480d17f0e97267378776bf477c630da8
Arian <arian@aryweb.nl>
2011-10-15 18:50:13 +0200

    Use classList when available

--- Source/Element/Class.js
+++ Source/Element/Class.js
@@ -1,29 +1,59 @@
 
 define(['../Element', 'Base/Utility/RegExp', 'Base/Utility/String'], function(Element, RegExp_, String){
 
-var classRegExps = {};
-var classRegExpOf = function(string){
-	return classRegExps[string] || (classRegExps[string] = new RegExp('(^|\\s)' + RegExp_.escape(string) + '(?:\\s|$)'));
+var has = function(feature){
+	if (feature == 'classlist'){
+		return 'classList' in document.documentElement;
+	}
 };
 
-return Element.implement({
+if (has('classlist')){
 
-	hasClass: function(className){
-		return classRegExpOf(className).test(this.node.className);
-	},
+	return Element.implement({
 
-	addClass: function(className){
-		var node = this.node;
-		if (!this.hasClass(className)) node.className = String.clean(node.className + ' ' + className);
-		return this;
-	},
+		hasClass: function(className){
+			return this.node.classList.contains(className);
+		},
 
-	removeClass: function(className){
-		var node = this.node;
-		node.className = String.clean(node.className.replace(classRegExpOf(className), '$1'));
-		return this;
-	}
+		addClass: function(className){
+			return this.node.classList.add(className);
+			return this;
+		},
 
-});
+		removeClass: function(className){
+			return this.node.classList.remove(className);
+			return this;
+		}
+
+	});
+
+} else {
+
+	var classRegExps = {};
+	var classRegExpOf = function(string){
+		return classRegExps[string] || (classRegExps[string] = new RegExp('(^|\\s)' + RegExp_.escape(string) + '(?:\\s|$)'));
+	};
+
+	return Element.implement({
+
+		hasClass: function(className){
+			return classRegExpOf(className).test(this.node.className);
+		},
+
+		addClass: function(className){
+			var node = this.node;
+			if (!this.hasClass(className)) node.className = String.clean(node.className + ' ' + className);
+			return this;
+		},
+
+		removeClass: function(className){
+			var node = this.node;
+			node.className = String.clean(node.className.replace(classRegExpOf(className), '$1'));
+			return this;
+		}
+
+	});
+
+}
 
 });
